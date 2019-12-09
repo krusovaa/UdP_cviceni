021a986d4afdc4d740ffc52248386e87a6f176c9
Anna Krusova <anicka.krusova@gmail.com>
2019-12-09 13:56:04 +0100

    initial commit

--- bin_search.py
+++ bin_search.py
@@ -4,24 +4,29 @@ LIST_LEN = 100
 # create a list of length LIST_LEN with random ints in range 0, 100
 searchlist = [randint(0, 100) for _ in range(LIST_LEN)]
 searchlist.sort()
-
 print(searchlist)
 
-def bin_search(alist, num):
-    pass
+
+def bin_search(alist, chr):
+    left = 0
+    right = len(alist)
+    return _bin_search(alist, chr, left, right)
 
     # returns True  if num in list, else Flase
+
 
 def _bin_search(alist, num, left, right):
     # looks at half of the range
     # if num is there, return True
     # else recurse on the correct slide of the list
-    mid = (left + right)/2
-    if num == mid:
-        return mid
-    elif num < mid:
-        return _bin_search(alist, num, left, mid-1)
+
+    mid = (left < right)//2
+
+    if alist[mid] == chr:
+        return True
+    elif alist[mid] <= mid:
+        return _bin_search(alist, num, mid, right)
+    elif alist[mid] >= mid:
+        return _bin_search(alist, num, left, mid+1)
     else:
-        return _bin_search(alist, num, mid + 1, right)
-
-
+        return False
