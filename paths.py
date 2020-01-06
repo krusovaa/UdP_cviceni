b41b770db69c9a3041af789d41f9cf1df8355533
Anna Krusova <anicka.krusova@gmail.com>
2020-01-06 18:06:03 +0100

    aktualizace

--- paths.py
+++ paths.py
@@ -1,7 +1,24 @@
 import pathlib
 
-p = pathlib.Path('/Users/zvukar/Desktop/B 4. ročník/Úvod do programování/cviceni/paths.py')
+p = pathlib.Path('C:/Users/zvukar/Desktop/B 4. ročník/Úvod do programování/cviceni/paths.py')
 p2 = pathlib.Path('paths.py')
 print(p)
 for par in p.parents:
     print(par)
+
+
+adir = p.parent
+for file in adir.iterdir():
+    if file.is_dir:
+        print("Dir {}".format(file))
+    else:
+        print(file)
+print(p.parent)
+
+cwd = pathlib.Path.cwd()
+in_dir = cwd / 'in'
+for file in in_dir.iterdir():
+    print(file)
+    with open (file, "r", encoding="utf-8") as f:
+        print(f.read())
+print(in_dir)
